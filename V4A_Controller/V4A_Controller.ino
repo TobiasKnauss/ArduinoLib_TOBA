d8e1bf4526f95a4d46ca276873a384b96edb4ffa
Tobias Knauss <tobias.knauss@web.de>
2026-05-08 19:55:53 +0200

    v.0.1.9000 in development.

---
+++ V4A_Controller/V4A_Controller.ino
@@ -0,0 +1,241 @@
+#include <Arduino.h>
+#include <Streaming.h>
+
+#include <STUC.h>
+#include <StucData.h>
+#include <WOCO.h>
+#include "WOCO_AliveCheck.h"
+#include "WOCO_DigitalPinMode.h"
+#include "WOCO_DigitalPinState.h"
+
+const uint8_t c_BufferDefaultValue = 0xDD;
+
+const uint16_t c_EepromOffset = 0;
+
+STUC* m_pSTUC = 0;
+WOCO* m_pWOCO = 0;
+
+uint8_t m_CommandBuffer[20];
+uint8_t m_PayloadSendBuffer[20];
+uint8_t m_PayloadRecvBuffer[20];
+uint8_t m_SendBuffer[50];
+uint8_t m_ReceiveBuffer[80];
+
+STUC::EChecksumType m_ChecksumType = STUC::EChecksumType::CRC16;
+
+uint8_t   m_CommandLength            = 0;
+uint8_t*  m_pCommandBufferCurrentPos = 0;
+uint16_t  m_ReceiveBufferWriteIndex  = 0;
+uint16_t  m_ReceiveBufferReadIndex   = 0;
+bool      m_DataAvailable            = false;
+StucData  m_RequestData;
+
+//--------------------------------------------------------------------
+void setup ()
+{
+  EResult result;
+
+  memset (m_PayloadSendBuffer, 0xFF, sizeof (m_PayloadSendBuffer));
+  memset (m_PayloadRecvBuffer, 0xFF, sizeof (m_PayloadRecvBuffer));
+  memset (m_SendBuffer       , 0xFF, sizeof (m_SendBuffer));
+  memset (m_ReceiveBuffer    , 0xFF, sizeof (m_ReceiveBuffer));
+
+  Serial.begin (9600);
+  Serial1.begin (9600);
+  delay (2000);
+
+  Serial << F("PayloadSendBuffer Addr=") << _HEX4((uint16_t)m_PayloadSendBuffer) << " Len=" << sizeof (m_PayloadSendBuffer) << endl;
+  Memory_PrintLn (m_PayloadSendBuffer, sizeof (m_PayloadSendBuffer));
+  Serial << F("PayloadRecvBuffer Addr=") << _HEX4((uint16_t)m_PayloadRecvBuffer) << " Len=" << sizeof (m_PayloadRecvBuffer) << endl;
+  Memory_PrintLn (m_PayloadRecvBuffer, sizeof (m_PayloadRecvBuffer));
+  Serial << F("SendBuffer        Addr=") << _HEX4((uint16_t)m_SendBuffer)        << " Len=" << sizeof (m_SendBuffer) << endl;
+  Memory_PrintLn (m_SendBuffer, sizeof (m_SendBuffer));
+  Serial << F("ReceiveBuffer     Addr=") << _HEX4((uint16_t)m_ReceiveBuffer)     << " Len=" << sizeof (m_ReceiveBuffer) << endl;
+  Memory_PrintLn (m_ReceiveBuffer, sizeof (m_ReceiveBuffer));
+
+  m_pSTUC = new STUC (c_EepromOffset, result);
+  Serial << F("STUC.ctor() result=") << STUC::GetResultText (result) << endl;
+}
+
+//--------------------------------------------------------------------
+void loop ()
+{
+  EResult result;
+
+  if (Serial.available ())
+  {
+    Serial << "Serial available: " << Serial.available () << endl;
+
+    // Receive all available data
+    while (Serial.available () > 0
+        && m_CommandLength < sizeof (m_CommandBuffer))
+    {
+      m_CommandBuffer[m_CommandLength++] = Serial.read ();
+    }
+  }
+
+  if (m_CommandLength > 0)
+  {
+    Serial << F("CommandBuffer: bytes used = ") << m_CommandLength << endl;
+    Memory_PrintLn (m_CommandBuffer, sizeof (m_CommandBuffer));
+
+    uint32_t workerDeviceId     = 0;
+    uint16_t wocoCommandNum     = 0;
+    bool     wocoActionIsWrite  = false;
+    uint8_t  pinNumber          = 0;
+    uint8_t  pinMode            = 0;
+    bool     pinState           = false;
+    RingBuffer_GetValueAndMovePtr (m_CommandBuffer, sizeof (m_CommandBuffer), m_pCommandBufferCurrentPos, workerDeviceId);
+    RingBuffer_GetValueAndMovePtr (m_CommandBuffer, sizeof (m_CommandBuffer), m_pCommandBufferCurrentPos, wocoCommandNum);
+    RingBuffer_GetValueAndMovePtr (m_CommandBuffer, sizeof (m_CommandBuffer), m_pCommandBufferCurrentPos, wocoActionIsWrite);
+    WOCO::ECommand wocoCommand = (WOCO::ECommand)wocoCommandNum;
+    switch (wocoCommand)
+    {
+    case WOCO::ECommand::AliveCheck:
+      m_pWOCO = (WOCO*)WOCO_AliveCheck::CreateReadRequest ();
+      break;
+
+    case WOCO::ECommand::DigitalPinMode:
+      RingBuffer_GetValueAndMovePtr (m_CommandBuffer, sizeof (m_CommandBuffer), m_pCommandBufferCurrentPos, pinNumber);
+      if (wocoActionIsWrite)
+      {
+        RingBuffer_GetValueAndMovePtr (m_CommandBuffer, sizeof (m_CommandBuffer), m_pCommandBufferCurrentPos, pinMode);
+        m_pWOCO = WOCO_DigitalPinMode::CreateWriteRequest (pinNumber, pinMode);
+      }
+      else
+        m_pWOCO = WOCO_DigitalPinMode::CreateReadRequest (pinNumber);
+      break;
+
+    case WOCO::ECommand::DigitalPinState:
+      RingBuffer_GetValueAndMovePtr (m_CommandBuffer, sizeof (m_CommandBuffer), m_pCommandBufferCurrentPos, pinNumber);
+      if (wocoActionIsWrite)
+      {
+        RingBuffer_GetValueAndMovePtr (m_CommandBuffer, sizeof (m_CommandBuffer), m_pCommandBufferCurrentPos, pinState);
+        m_pWOCO = WOCO_DigitalPinState::CreateWriteRequest (pinNumber, pinState);
+      }
+      else
+        m_pWOCO = WOCO_DigitalPinState::CreateReadRequest (pinNumber);
+      break;
+    }
+
+    uint8_t payloadLength = 0;
+    result = m_pWOCO->ComposePayload (m_PayloadSendBuffer,
+                                      sizeof (m_PayloadSendBuffer),
+                                      payloadLength);
+    Serial << F("WOCO->ComposePayload() result=") << WOCO::GetResultText (result) << endl;
+
+    Serial << F("PayloadSendBuffer: bytes used = ") << payloadLength << endl;
+    Memory_PrintLn (m_PayloadSendBuffer, sizeof (m_PayloadSendBuffer));
+
+    m_RequestData = StucData (m_pWOCO->GetActionIsWrite (),
+                              workerDeviceId,
+                              (uint8_t)m_pWOCO->GetCommand ());
+    m_RequestData.SetPayloadInfo (m_PayloadSendBuffer,
+                                  sizeof (m_PayloadSendBuffer),
+                                  payloadLength);
+    uint16_t requestMessageLength = 0;
+
+    result = m_pSTUC->ComposeRequest (m_RequestData,
+                                      m_SendBuffer,
+                                      sizeof (m_SendBuffer),
+                                      requestMessageLength);
+    Serial << F("STUC.ComposeRequest() result=") << STUC::GetResultText (result) << endl;
+
+    Serial << F("SendBuffer: bytes used = ") << requestMessageLength << endl;
+    Memory_PrintLn (m_SendBuffer, sizeof (m_SendBuffer));
+
+    if (result == EResult::SUCCESS)
+    {
+      Serial << F("Sending data...") << endl;
+      Serial1.write (m_SendBuffer, requestMessageLength);
+      Serial1.flush ();
+    }
+
+    memset (m_CommandBuffer, c_BufferDefaultValue, m_CommandLength);
+    Serial << F("CommandBuffer:") << endl;
+    Memory_PrintLn (m_CommandBuffer, sizeof (m_CommandBuffer));
+    m_CommandLength = 0;
+
+    memset (m_PayloadSendBuffer, c_BufferDefaultValue, payloadLength);
+    Serial << F("PayloadSendBuffer:") << endl;
+    Memory_PrintLn (m_PayloadSendBuffer, sizeof (m_PayloadSendBuffer));
+
+    memset (m_SendBuffer, c_BufferDefaultValue, requestMessageLength);
+    Serial << F("SendBuffer:") << endl;
+    Memory_PrintLn (m_SendBuffer, sizeof (m_SendBuffer));
+  }
+
+  if (Serial1.available ())
+  {
+    Serial << "Serial1 available: " << Serial1.available () << endl;
+
+    // Receive all available data
+    while (Serial1.available ())
+    {
+      m_ReceiveBuffer[m_ReceiveBufferWriteIndex++] = Serial1.read ();
+      if (m_ReceiveBufferWriteIndex >= sizeof (m_ReceiveBuffer))
+        m_ReceiveBufferWriteIndex = 0;
+    }
+
+    Serial << F("ReceiveBuffer: position = ") << m_ReceiveBufferWriteIndex << endl;
+    Memory_PrintLn (m_ReceiveBuffer, sizeof (m_ReceiveBuffer));
+
+    m_DataAvailable = true;
+  }
+
+  if (m_DataAvailable)
+  {
+    bool     receivedMessageTypeIsReply = false;
+    uint8_t  receivedMessageLength      = 0;
+    StucData receivedData;
+    receivedData.SetPayloadInfo (m_PayloadRecvBuffer,
+                                 sizeof (m_PayloadRecvBuffer));
+
+    // Analyse message in the received data
+    result = m_pSTUC->AnalyseMessage (m_ReceiveBuffer,
+                                      sizeof (m_ReceiveBuffer),
+                                      m_ReceiveBufferReadIndex,
+                                      receivedData,
+                                      receivedMessageTypeIsReply,
+                                      receivedMessageLength);
+    Serial << F("STUC.AnalyseMessage() result=") << STUC::GetResultText (result) << endl;
+
+    Serial << F("Message Type is Reply: ") << receivedMessageTypeIsReply          << endl;
+    Serial << F("Action is Write:       ") << receivedData.ActionIsWrite          << endl;
+    Serial << F("Remote Device Id:      ") << _HEX8 (receivedData.RemoteDeviceId) << endl;
+    Serial << F("Message Id:            ") << receivedData.MessageId              << endl;
+    Serial << F("Timestamp:             ") << receivedData.Timestamp              << endl;
+    Serial << F("CommandId:             ") << receivedData.CommandId              << endl;
+    Serial << F("Result:                ") << (uint8_t)receivedData.MessageResult << endl;
+    Serial << F("Payload Data Length:   ") << receivedData.PayloadLength          << endl;
+    Serial << F("Message Length:        ") << receivedMessageLength               << endl;
+
+    Serial << F("PayloadRecvBuffer: bytes used = ") << receivedData.PayloadLength << endl;
+    Memory_PrintLn (m_PayloadRecvBuffer, sizeof (m_PayloadRecvBuffer));
+
+    if (result == EResult::SUCCESS)
+    {
+      RingBuffer_SetValueBackward (m_ReceiveBuffer,
+                                   sizeof (m_ReceiveBuffer),
+                                   m_ReceiveBufferReadIndex,
+                                   receivedMessageLength,
+                                   c_BufferDefaultValue);
+      Serial << F("ReceiveBuffer:") << endl;
+      Memory_PrintLn (m_ReceiveBuffer, sizeof (m_ReceiveBuffer));
+
+      // Evaluate reply
+
+      memset (m_PayloadRecvBuffer, c_BufferDefaultValue, receivedData.PayloadLength);
+      Serial << F("PayloadRecvBuffer:") << endl;
+      Memory_PrintLn (m_PayloadRecvBuffer, sizeof (m_PayloadRecvBuffer));
+    }
+    else
+      m_DataAvailable = false;
+  }
+
+  if (!m_DataAvailable)
+  {
+    Serial.println ("Idle.");
+    delay (1000);
+  }
+}
---
+++ V4A_Worker/V4A_Worker.ino
@@ -0,0 +1,263 @@
+#include <Arduino.h>
+#include <Streaming.h>
+
+#include <STUC.h>
+#include <StucData.h>
+#include <WOCO.h>
+#include <WOCO_AliveCheck.h>
+#include <WOCO_DigitalPinState.h>
+#include <WOCO_DigitalPinMode.h>
+
+const uint8_t c_BufferDefaultValue = 0xDD;
+const uint16_t c_EepromOffset = 0;
+
+STUC* m_pSTUC = 0;
+WOCO* m_pWOCO = 0;  // Command
+
+uint8_t m_PayloadSendBuffer[20];
+uint8_t m_PayloadRecvBuffer[20];
+uint8_t m_SendBuffer[50];
+uint8_t m_ReceiveBuffer[80];
+
+uint16_t  m_ReceiveBufferWriteIndex = 0;
+uint16_t  m_ReceiveBufferReadIndex  = 0;
+bool      m_DataAvailable           = false;
+bool      m_IsWorking               = false;
+StucData  m_RequestData;
+
+//--------------------------------------------------------------------
+void setup ()
+{
+  EResult result;
+
+  memset (m_PayloadSendBuffer, 0xFF, sizeof (m_PayloadSendBuffer));
+  memset (m_PayloadRecvBuffer, 0xFF, sizeof (m_PayloadRecvBuffer));
+  memset (m_SendBuffer       , 0xFF, sizeof (m_SendBuffer));
+  memset (m_ReceiveBuffer    , 0xFF, sizeof (m_ReceiveBuffer));
+
+  Serial.begin (9600);
+  Serial1.begin (9600);
+  delay (2000);
+
+  Serial << F("PayloadSendBuffer Addr=") << _HEX4((uint16_t)m_PayloadSendBuffer) << " Len=" << sizeof (m_PayloadSendBuffer) << endl;
+  Memory_PrintLn (m_PayloadSendBuffer, sizeof (m_PayloadSendBuffer));
+  Serial << F("PayloadRecvBuffer Addr=") << _HEX4((uint16_t)m_PayloadRecvBuffer) << " Len=" << sizeof (m_PayloadRecvBuffer) << endl;
+  Memory_PrintLn (m_PayloadRecvBuffer, sizeof (m_PayloadRecvBuffer));
+  Serial << F("SendBuffer        Addr=") << _HEX4((uint16_t)m_SendBuffer)        << " Len=" << sizeof (m_SendBuffer) << endl;
+  Memory_PrintLn (m_SendBuffer, sizeof (m_SendBuffer));
+  Serial << F("ReceiveBuffer     Addr=") << _HEX4((uint16_t)m_ReceiveBuffer)     << " Len=" << sizeof (m_ReceiveBuffer) << endl;
+  Memory_PrintLn (m_ReceiveBuffer, sizeof (m_ReceiveBuffer));
+
+  m_pSTUC = new STUC (c_EepromOffset, result);
+  Serial << F("STUC.ctor() result=") << STUC::GetResultText (result) << endl;
+}
+
+//--------------------------------------------------------------------
+void loop ()
+{
+  EResult result;
+
+  if (Serial1.available ()
+  &&  !m_IsWorking)
+  {
+    Serial << "Serial1 available: " << Serial1.available () << endl;
+
+    // Receive all available data
+    while (Serial1.available ())
+    {
+      m_ReceiveBuffer[m_ReceiveBufferWriteIndex++] = Serial1.read ();
+      if (m_ReceiveBufferWriteIndex >= sizeof (m_ReceiveBuffer))
+        m_ReceiveBufferWriteIndex = 0;
+    }
+
+    Serial << F("ReceiveBuffer: position = ") << m_ReceiveBufferWriteIndex << endl;
+    Memory_PrintLn (m_ReceiveBuffer, sizeof (m_ReceiveBuffer));
+
+    m_DataAvailable = true;
+  }
+
+  if (m_DataAvailable
+  &&  !m_IsWorking)
+  {
+    bool     receivedMessageTypeIsReply = false;
+    uint8_t  receivedMessageLength      = 0;
+    StucData receivedData;
+    receivedData.SetPayloadInfo (m_PayloadRecvBuffer,
+                                 sizeof (m_PayloadRecvBuffer));
+
+    // Analyse message in the received data
+    result = m_pSTUC->AnalyseMessage (m_ReceiveBuffer,
+                                      sizeof (m_ReceiveBuffer),
+                                      m_ReceiveBufferReadIndex,
+                                      receivedData,
+                                      receivedMessageTypeIsReply,
+                                      receivedMessageLength);
+    Serial << F("STUC.AnalyseMessage() result=") << STUC::GetResultText (result) << endl;
+
+    Serial << F("Message Type is Reply: ") << receivedMessageTypeIsReply          << endl;
+    Serial << F("Action is Write:       ") << receivedData.ActionIsWrite          << endl;
+    Serial << F("Remote Device Id:      ") << _HEX8 (receivedData.RemoteDeviceId) << endl;
+    Serial << F("Message Id:            ") << receivedData.MessageId              << endl;
+    Serial << F("Timestamp:             ") << receivedData.Timestamp              << endl;
+    Serial << F("CommandId:             ") << receivedData.CommandId              << endl;
+    Serial << F("Result:                ") << (uint8_t)receivedData.MessageResult << endl;
+    Serial << F("Payload Data Length:   ") << receivedData.PayloadLength          << endl;
+    Serial << F("Message Length:        ") << receivedMessageLength               << endl;
+
+    Serial << F("PayloadRecvBuffer: bytes used = ") << receivedData.PayloadLength << endl;
+    Memory_PrintLn (m_PayloadRecvBuffer, sizeof (m_PayloadRecvBuffer));
+
+    if (result == EResult::SUCCESS)
+    {
+      m_RequestData = receivedData;
+
+      RingBuffer_SetValueBackward (m_ReceiveBuffer,
+                                   sizeof (m_ReceiveBuffer),
+                                   m_ReceiveBufferReadIndex,
+                                   receivedMessageLength,
+                                   c_BufferDefaultValue);
+      Serial << F("ReceiveBuffer:") << endl;
+      Memory_PrintLn (m_ReceiveBuffer, sizeof (m_ReceiveBuffer));
+
+      if (!receivedMessageTypeIsReply)
+      {
+        Serial << F("Message is a request. Analyze Command...");
+        m_IsWorking = true;
+
+        WOCO* pWOCO = 0;
+        result = WOCO::Create ((WOCO::ECommand)m_RequestData.CommandId,
+                               receivedMessageTypeIsReply,
+                               m_RequestData.ActionIsWrite,
+                               pWOCO);
+        Serial << F("WOCO.Create() result=") << WOCO::GetResultText (result) << endl;
+
+        STUC::EMessageResult messageResult = STUC::EMessageResult::None;
+        if (result == EResult::SUCCESS)
+        {
+          result = pWOCO->AnalyzePayload (m_RequestData.pPayloadBuffer,
+                                          m_RequestData.PayloadBufferLength,
+                                          m_RequestData.PayloadLength);
+          Serial << F("WOCO.AnalyzePayload() result=") << WOCO::GetResultText (result) << endl;
+        }
+        else
+          messageResult = STUC::EMessageResult::FAIL_CommandIdNotSupported
+
+
+        if (result == EResult::SUCCESS)
+        {
+          Serial << F("Command is valid. Start working...");
+          m_pWOCO = pWOCO;
+          m_IsWorking = true;
+        }
+
+        if (result != EResult::SUCCESS)
+        {
+          StucData replyData = StucData (m_RequestData.ActionIsWrite,
+                                         m_RequestData.RemoteDeviceId,
+                                         m_RequestData.MessageId,,
+                                         0,
+                                         m_RequestData.CommandId,
+                                         messageResult)
+        }
+      }
+      else
+      {
+        Serial << F("Message is no request. Nothing to do.");
+        // Sending a reply is useless since no reply is expected.
+      }
+    }
+    else
+    {
+      m_DataAvailable = false;
+
+      STUC::EMessageResult messageResult = STUC::GetMessageResultForAnalysisResult (result);
+      if (messageResult != STUC::EMessageResult::None)
+      {
+        m_ReplyData = StucData (m_RequestData.ActionIsWrite,
+                                m_RequestData.RemoteDeviceId,
+                                m_RequestData.MessageId,
+                                m_RequestData.Timestamp,
+                                m_RequestData.CommandId,
+                                messageResult);
+      }
+    }
+
+    memset (m_PayloadRecvBuffer, c_BufferDefaultValue, m_RequestData.PayloadLength);
+    Serial << F("PayloadRecvBuffer:") << endl;
+    Memory_PrintLn (m_PayloadRecvBuffer, sizeof (m_PayloadRecvBuffer));
+  }
+
+  if (m_IsWorking)
+  {
+    WOCO* pWORE = 0;  // Reply
+    switch (m_pWOCO->GetCommand ())
+    {
+    case WOCO::ECommand::AliveCheck:
+      pWORE = WOCO_AliveCheck::CreateReadReply ();
+      break;
+
+    case WOCO::ECommand::DigitalPinMode:
+      {
+        WOCO_DigitalPinMode* pWOCO_DigitalPinMode = (WOCO_DigitalPinMode*)m_pWOCO;
+        pinMode (uint8_t pin, uint8_t mode)
+        pWOCO_DigitalPinMode->GetPinMode()
+      }
+      break;
+
+    case WOCO::ECommand::DigitalPinState:
+      {
+        WOCO_DigitalPinState* pWOCO_DigitalPinState = (WOCO_DigitalPinState*)m_pWOCO;
+      }
+      break;
+
+    default:
+      
+      break;
+    }
+
+    Serial << F("PayloadSendBuffer:") << endl;
+    Memory_PrintLn (m_PayloadSendBuffer, sizeof (m_PayloadSendBuffer));
+
+    StucData replyData = StucData (m_RequestData.ActionIsWrite,
+                                   m_RequestData.RemoteDeviceId,
+                                   m_RequestData.MessageId,
+                                   m_RequestData.Timestamp,
+                                   m_RequestData.CommandId,
+                                   STUC::EMessageResult::Success);
+    replyData.SetPayloadInfo (m_PayloadSendBuffer, sizeof (m_PayloadSendBuffer), m_RequestData.PayloadLength);
+
+    uint16_t replyMessageLength = 0;
+    result = m_pSTUC->ComposeReply (replyData,
+                                    m_SendBuffer,
+                                    sizeof (m_SendBuffer),
+                                    replyMessageLength);
+    Serial << F("STUC.ComposeReply() result=") << STUC::GetResultText (result) << endl;
+
+    Serial << F("SendBuffer: bytes used = ") << replyMessageLength << endl;
+    Memory_PrintLn (m_SendBuffer, sizeof (m_SendBuffer));
+
+    if (result == EResult::SUCCESS)
+    {
+      Serial1.write (m_SendBuffer, replyMessageLength);
+      Serial1.flush ();
+    }
+
+    memset (m_PayloadSendBuffer, c_BufferDefaultValue, replyData.PayloadLength);
+    memset (m_SendBuffer,        c_BufferDefaultValue, replyMessageLength);
+
+    m_IsWorking = false;
+    m_pWOCO = 0;
+  }
+
+  if (m_ReplyData != 0)
+  {
+
+  }
+  
+  if (!m_DataAvailable
+  &&  !m_IsWorking)
+  {
+    Serial.println ("Idle.");
+    delay (1000);
+  }
+}
